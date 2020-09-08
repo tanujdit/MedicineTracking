d8125e7d570127bc3d659431a46954ff7df6f47e
Cloud <tanujdit@gmail.com>
2020-09-08 16:46:43 +0530

    Initial commit

---
+++ MedicineTrackingSol/MedicineTrackingApp/Migrations/20200908061643_Initial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MedicineTrackingApp.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Medicine",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MedicineName = table.Column<string>(nullable: true),
+                    Brand = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false),
+                    ExpiryDate = table.Column<DateTime>(nullable: false),
+                    Notes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicine", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Medicine");
+        }
+    }
+}
---
+++ MedicineTrackingSol/MedicineTrackingApp/Models/Medicine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MedicineTrackingApp.Models
+{
+    public class Medicine
+    {
+        public int Id { get; set; }
+        [Required]
+        public string MedicineName { get; set; }
+        [Required]
+        public string Brand { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public DateTime ExpiryDate { get; set; }
+
+        public string Notes { get; set; }
+        
+    }
+}
